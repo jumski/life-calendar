5617a6d8cd92858f9bb2f1c79466041975ac68fb
Wojtek Majewski <jumski@gmail.com>
2020-04-24 13:43:47 +0200

    Add simple test (not working as expected yet)

--- src/jumski/life_calendar/workspaces/calendar_cards.cljs
+++ src/jumski/life_calendar/workspaces/calendar_cards.cljs
@@ -1,13 +1,15 @@
 (ns jumski.life-calendar.workspaces.calendar-cards
   (:require [nubank.workspaces.core :as ws]
             [nubank.workspaces.card-types.react :as ct.react]
-            [reagent.core :as reagent]))
+            [reagent.core :as reagent]
+            [cljs.test :refer-macros [is testing]]))
 
 (defn my-component [& args]
-  [:div [:p "om23ghax"]])
+  [:div [:p "om3ghax"]])
 
 (ws/defcard hello-card
   (ct.react/react-card
     (reagent/as-element [my-component])))
 
-
+(ws/deftest sample-test
+  (is (= 1 2)))
